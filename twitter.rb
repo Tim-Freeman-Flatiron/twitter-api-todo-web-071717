571e69b2005302bec2ccb8a1267fd2820914f29f
Tim Freeman <d.timothy.freeman@gmail.com>
2017-07-24 13:03:49 -0400

    done

--- twitter.rb
+++ twitter.rb
@@ -17,18 +17,31 @@ class TwitterApi
 
   def most_recent_friend
     #find the twitter gem method that retrieves a user's friends and grab the most recent friend
+    client.friends.first
   end
 
   def find_user_for(username)
     #find the twitter gem method that returns the correct user, given a username
+    client.user(username)
   end
 
   def find_followers_for(username)
     #find the twitter gem method that returns the follows of a given user
+    followers = client.followers(username)
+    i = 0
+    collection = []
+    while i <= 9 
+      followers.each do |follower|
+        collection << follower
+        i += 1
+      end
+    end
+    collection
   end
 
   def homepage_timeline
     #find the twitter gem method that retreives the tweets from a user's timeline.
+    client.user_timeline
   end
   
 end
